08418343cc9c0228fc9f910c5c069187e8fc8c67
HomieZ09 <phumsoen123@gmail.com>
2025-04-12 22:41:28 +0700

    docs: docs(swagger): add bearer and accessCode auth schemes to Swagger UI

--- src/main/java/com/buffetpos/backend/config/SwaggerConfig.java
+++ src/main/java/com/buffetpos/backend/config/SwaggerConfig.java
@@ -1,12 +1,17 @@
 package com.buffetpos.backend.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    final String bearerScheme = "bearerAuth";
+    final String accessCodeScheme = "accessCode";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
@@ -15,7 +20,19 @@ public class SwaggerConfig {
                         .version("1.0")
                         .description("API documentation for BuffetPOS"))
                 .addTagsItem(new Tag().name("General").description("General endpoints"))
-                .addTagsItem(new Tag().name("Authentication").description("Authentication endpoints")
-                );
+                .addTagsItem(new Tag().name("Authentication").description("Authentication endpoints"))
+                .addTagsItem(new Tag().name("Category Manager").description("Category endpoints"))
+                .addSecurityItem(new SecurityRequirement().addList(bearerScheme))
+                .addSecurityItem(new SecurityRequirement().addList(accessCodeScheme))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(bearerScheme, new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"))
+                        .addSecuritySchemes(accessCodeScheme, new SecurityScheme()
+                                .name("AccessCode")
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)));
     }
 }
--- src/main/java/com/buffetpos/backend/controllers/CategoryController.java
+++ src/main/java/com/buffetpos/backend/controllers/CategoryController.java
@@ -1,6 +1,8 @@
 package com.buffetpos.backend.controllers;
 
 import com.buffetpos.backend.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,21 +11,37 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/categories")
 @RequiredArgsConstructor
+@Tag( name="Category Manager" )
 public class CategoryController {
     private final CategoryService categoryService;
 
+    @Operation(
+            summary = "Create Category",
+            description = "Creates a new category"
+    )
+    @SecurityRequirement(name = "bearerAuth")
     @PostMapping("/")
     public ResponseEntity<?> createCategory(String name) {
         categoryService.createCategory(name);
         return ResponseEntity.created(null).body("Category created successfully");
     }
 
+    @Operation(
+            summary = "Remove Category",
+            description = "Removes a category"
+    )
+    @SecurityRequirement(name = "bearerAuth")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> removeCategory(Long id) {
         categoryService.removeCategory(id);
         return ResponseEntity.ok("Category removed successfully");
     }
 
+    @Operation(
+            summary = "Update Category",
+            description = "Updates a category"
+    )
+    @SecurityRequirement(name = "bearerAuth")
     @PutMapping("/{id}")
     public ResponseEntity<?> updateCategory(@PathVariable Long id, String name) {
         categoryService.updateCategory(id, name);
