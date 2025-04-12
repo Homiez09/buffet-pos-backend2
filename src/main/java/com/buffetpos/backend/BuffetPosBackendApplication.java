fab74a3a37c5a6572fbed25685eae9ea75205317
HomieZ09 <phumsoen123@gmail.com>
2025-04-12 17:54:27 +0700

    Initial commit

---
+++ src/main/java/com/buffetpos/backend/BuffetPosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.buffetpos.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetPosBackendApplication.class, args);
+	}
+
+}
